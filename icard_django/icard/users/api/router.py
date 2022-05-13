19a207774b8e6272815fc1308daad6ae53ca19de
Julian Valencia <valenciajulian657@gmail.com>
2022-05-12 21:54:10 -0500

    login y CORS

--- icard_django/icard/users/api/router.py
+++ icard_django/icard/users/api/router.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.view import TokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView
+)
 from users.api.views import UserApipViewSet, UserView
 
 router_user = DefaultRouter()
