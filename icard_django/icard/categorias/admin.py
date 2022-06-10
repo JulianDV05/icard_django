d7f1863e59e2336532dd3cdde7d606c3ac28716a
Juan Felipe Sanchez Roa <felipe41sanchez@gmail.com>
2022-06-10 16:32:12 -0500

    sección 8 APP Categorias

---
+++ icard_django/icard/categorias/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.contrib import admin
+from categorias.models import Category
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
