2873500e39952a4aea012e12cb9670f8572f1100
Julian Valencia <valenciajulian657@gmail.com>
2022-06-23 18:20:50 -0500

    Nuevamente Backend de sección 8

--- icard_django/icard/icard/settings.py
+++ icard_django/icard/icard/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'corsheaders',
     'users',
+    'categories'
 ]
 
 MIDDLEWARE = [
@@ -128,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL ='/uploads/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'uploads')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
