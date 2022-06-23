eb797fab13c90e7edd33651bd0a8369f203fbd39
Julian Valencia <valenciajulian657@gmail.com>
2022-06-23 15:49:39 -0500

    retraso por error en categorias

--- icard_django/icard/icard/urls.py
+++ icard_django/icard/icard/urls.py
@@ -16,12 +16,9 @@ Including another URLconf
 from django import urls
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from users.api.router import router_user
-from categorias.api.router import router_category
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -43,7 +40,4 @@ urlpatterns = [
          cache_timeout=0), name='schema-redoc'),
     path('api/', include('users.api.router')),
     path('api/', include(router_user.urls)),
-    path('api/', include(router_category.urls)),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
