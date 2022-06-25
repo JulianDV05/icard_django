a328f33cdfb336c31a7ad0df5e8e2dcfa47f96e0
Kike Borda <danenrborsan2@gmail.com>
2022-06-24 22:35:11 -0500

    seccion 9 y 10

--- icard_react/src/pages/Admin/CategoriesAdmin.js
+++ icard_react/src/pages/Admin/CategoriesAdmin.js
@@ -5,7 +5,7 @@ import {
   TableCategoryAdmin,
   AddEditCategoryForm,
 } from "../../components/Admin";
-import { ModalBasic } from "../../components/common";
+import { ModalBasic } from "../../components/Common";
 import { useCategory } from "../../hooks";
 
 export function CategoriesAdmin() {
